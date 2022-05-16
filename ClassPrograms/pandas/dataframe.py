344f88525cf2c7c9bc1b1a3f20326d3104d7bbf9
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-05-16 04:35:17 +0000

    pandas fun

--- ClassPrograms/pandas/dataframe.py
+++ ClassPrograms/pandas/dataframe.py
@@ -3,8 +3,10 @@ import pandas as pd
 data = {
   'Names' : ['Sreemannarayana','Akshit','Abhishek','Suraj'],
   'Specialisation' : ['Database, Algorithm, and Support in other departments', 'Framework and Management', 'Frontend God', 'Algo Guy'],
-  'Age' : [18]*4
+  'Age' : [19, 18, 18, 18]
 }
 
-df = pd.DataFrame(data)
-print(df.dtypes)
+df = pd.DataFrame(data, index = ['a','b','c','d'], columns = ['Names','Age'])
+# altDf = df.loc[lambda df : df['Age'] > 18]
+altDf = df[df['Age']>18]
+print(altDf)
--- ClassPrograms/pandas/excel.py
+++ ClassPrograms/pandas/excel.py
@@ -1,22 +1,20 @@
 import pandas as pd
 
-file = pd.read_excel('FinancialSample.xlsx')
-df = pd.DataFrame(file)
 data = {
-  'Segment' : ['Government'],
-  'Country' : ['Russia'],
-  'Product' : ['S400'],
-  'Discount Band' : ['Medium'],
-  'Units Sold' : ['100'],
-  'Manufacturing Price' : ['1000000000'],
-  'Sale Price' : ['9000'],
-  'Gross Sales' : ['100000000000'],
-  'Discounts' :   ['900000'],
-  ' Sales' : ['99,999,100,000']
+  'Segment' : 'Government',
+  'Country' : 'Russia',
+  'Product' : 'S400',
+  'Discount Band' : 'Medium',
+  'Units Sold' : [100],
+  'Manufacturing Price' : [1000000000],
+  'Sale Price' : [9000],
+  'Gross Sales' : [100000000000],
+  'Discounts' :   [900000],
+  ' Sales' : [99999100000]
 }
 df2 = pd.DataFrame(data)
 
 
-with pd.ExcelWriter('FinancialSample.xlsx') as writer:
+with pd.ExcelWriter('FinancialSample1.xlsx') as writer:
   df2.to_excel(writer, sheet_name='new_sheet2')
-print(df.dtypes)
+print(df2.dtypes)
