1b2815b73f7ab9fbdb29545e40d193e380a88491
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-05-19 10:55:47 +0000

    Thu, May 19, 2022, 10:55 AM +00:00

--- ClassPrograms/JSON/introToJSON.py
+++ ClassPrograms/JSON/introToJSON.py
@@ -3,4 +3,12 @@
 ''' Link for more details : https://quickref.me/json'''
 ''' Link to know how to work with JSON using python : https://www.w3schools.com/python/python_json.asp '''
 
-example = 
+
+# Code Here
+import json
+
+example = {
+    "Name" : "Java Script",
+    "Age" : 27,
+    "Qualities" : []
+}
