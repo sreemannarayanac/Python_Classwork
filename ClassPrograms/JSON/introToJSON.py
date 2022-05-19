87128423f098ab0188975aecadb41d3d355c1a81
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-05-19 10:49:31 +0000

    Thu, May 19, 2022, 10:49 AM +00:00

---
+++ ClassPrograms/JSON/introToJSON.py
@@ -0,0 +1,3 @@
+# JSON is used to store unstructured data. JSON stands for JavaScript Object Notation
+# Other methods which can be used are YAML and XML
+''' Link for more details : https://quickref.me/json'''
