b4a5443ce6741d979497a7519d3bcf94431a9d85
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-06-01 14:25:22 +0000

    Wed, Jun 1, 2022, 2:25 PM +00:00

--- ClassPrograms/pandas/problem.py
+++ ClassPrograms/pandas/problem.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+record = {
+    'id':[1,2,3,4,5,6,7,8,9,10],
+    'Names':['abc','def','ghi','jkl','mno','pqr','stu','vwx','yza','xyz'],
+    'Age':[24,45,43,56,78,32,12,19,54,65]
+}
+
+df = pd.DataFrame(record)
+
