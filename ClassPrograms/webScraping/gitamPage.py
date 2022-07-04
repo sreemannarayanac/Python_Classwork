55b663e07fe83d1fab0123ad19d6de5db2fc07ce
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-07-04 08:47:03 +0000

    Mon, Jul 4, 2022, 8:47 AM +00:00

--- ClassPrograms/webScraping/gitamPage.py
+++ ClassPrograms/webScraping/gitamPage.py
@@ -5,5 +5,9 @@ page = rs.get('https://www.gitam.edu/')
 src = page.content
 soup = bs(src, 'lxml')
 
+
+programs = []
+
 for h2_tag in soup.find_all('h2'):
-    
+    if h2_tag.text == "Admissions":
+        
