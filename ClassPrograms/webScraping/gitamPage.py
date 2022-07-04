d00a183dc5bfc5fcfbfb8653fad28ddcfad1cefc
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-07-04 08:45:20 +0000

    Mon, Jul 4, 2022, 8:45 AM +00:00

--- ClassPrograms/webScraping/gitamPage.py
+++ ClassPrograms/webScraping/gitamPage.py
@@ -2,4 +2,8 @@ import requests as rs
 from bs4 import BeautifulSoup as bs
 
 page = rs.get('https://www.gitam.edu/')
+src = page.content
+soup = bs(src, 'lxml')
 
+for h2_tag in soup.find_all('h2'):
+    
