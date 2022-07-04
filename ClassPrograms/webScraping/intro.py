080ab31cc2c500734c4e1c7ca25c05fd9c383f72
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-07-04 08:18:45 +0000

    Mon, Jul 4, 2022, 8:18 AM +00:00

--- ClassPrograms/webScraping/intro.py
+++ ClassPrograms/webScraping/intro.py
@@ -1,6 +1,13 @@
 import requests as rs
 from bs4 import BeautifulSoup as bs
 
-result = rs.get('https://www.google.com/')
+result = rs.get('https://www.gitam.edu/')
 
-print(result.status_code)
+# print(result.status_code) if 200 page is opening
+
+src = result.content # Gets source code
+
+soup = bs(result, 'lxml')
+
+headers = soup.findall('h1')
+print(headers)
